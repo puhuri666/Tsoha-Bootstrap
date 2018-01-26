01b50865cbeacbbb8553529969ed2c4b336ba200
puhuri666 <pmarkkola@gmail.com>
2018-01-26 16:50:14 +0200

    päivitys

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,56 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO bettor(firstname, lastname, address, zipcode, town, email, password, superuser)
+VALUES('Petteri', 'Markkola', 'Ouninpohjankummuntie 5', '00100', 'Helsinki', 'petteri.markkola@hki.fi', 'Apinaorkesteri5', TRUE);
+
+INSERT INTO bettor(firstname, lastname, address, zipcode, town, email, password, balance)
+VALUES('Salama', 'Härri', 'Liskotie 5 D 4', '00150', 'Helsinki', 'salamahaerri@sposti.fi', 'meikanpassu', 139.5);
+
+INSERT INTO bettor(firstname, lastname, address, zipcode, town, email, password, balance)
+VALUES('Mc', 'Megaputti', 'Märssytie 1 F 6', '90560', 'Oulu', 'mega@putti.fi', 'PassW0rdZ', 880.4);
+
+INSERT INTO bettor(firstname, lastname, address, zipcode, town, email, password, balance)
+VALUES('Raivo', 'Raimo', 'Kirkkokatu 16 B 12', '80100', 'Joensuu', 'raivo@raipe.fi', 'zSKALsaa2', 1233.50);
+
+INSERT INTO team(name, sport, league) VALUES('Kärpät', 'hockey', 'smliiga');
+INSERT INTO team(name, sport, league) VALUES('HIFK', 'hockey', 'smliiga');
+INSERT INTO team(name, sport, league) VALUES('Tappara', 'hockey', 'smliiga');
+INSERT INTO team(name, sport, league) VALUES('Ilves', 'hockey', 'smliiga');
+INSERT INTO team(name, sport, league) VALUES('Lukko', 'hockey', 'smliiga');
+INSERT INTO team(name, sport, league) VALUES('HPK', 'hockey', 'smliiga');
+INSERT INTO team(name, sport, league) VALUES('Florida', 'hockey', 'nhl');
+INSERT INTO team(name, sport, league) VALUES('Anaheim', 'hockey', 'nhl');
+INSERT INTO team(name, sport, league) VALUES('NY Islanders', 'hockey', 'nhl');
+INSERT INTO team(name, sport, league) VALUES('NY Rangers', 'hockey', 'nhl');
+INSERT INTO team(name, sport, league) VALUES('Minnesota', 'hockey', 'nhl');
+INSERT INTO team(name, sport, league) VALUES('Tampa Bay', 'hockey', 'nhl');
+
+INSERT INTO suggestion(suggestion, bettor)
+VALUES ('laittakaa futistakin, senkin kendojanarit!!!!', 3);
+
+INSERT INTO matchup(startdate, scorehome, scoreaway, result, betting_result)
+VALUES('2018-01-26', 5, 1, '5-1', 1);
+INSERT INTO matchup(startdate, scorehome, scoreaway, result, betting_result)
+VALUES('2018-01-24', 5, 6, '5-6', 2);
+INSERT INTO matchup(startdate, scorehome, scoreaway, result, betting_result)
+VALUES('2018-01-23', 4, 9, '4-9', 1);
+
+INSERT INTO matchup_team(matchup, team)
+VALUES(1, 1);
+INSERT INTO matchup_team(matchup, team, hometeam)
+VALUES(1, 2, FALSE);
+
+INSERT INTO matchup_team(matchup, team)
+VALUES(2, 3);
+INSERT INTO matchup_team(matchup, team, hometeam)
+VALUES(2, 4, FALSE);
+
+INSERT INTO matchup_team(matchup, team)
+VALUES(2, 5);
+INSERT INTO matchup_team(matchup, team, hometeam)
+VALUES(2, 6, FALSE);
+
+INSERT INTO wager(bettor, matchup, betting_choice, betting_amount)
+VALUES (4, 2, 'X', 250);
+
+INSERT INTO wager(bettor, matchup, betting_choice, betting_amount)
+VALUES (3, 1, '1', 550.65);
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,49 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE bettor (
+    id SERIAL PRIMARY KEY,
+    firstname varchar(50) NOT NULL,
+    lastname varchar(50) NOT NULL,
+    address varchar(150) NOT NULL,
+    zipcode varchar(20) NOT NULL,
+    town varchar(50) NOT NULL,
+    email varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
+    balance float NOT NULL DEFAULT 0.0,
+    superuser boolean DEFAULT FALSE NOT NULL
+);
+
+
+CREATE TABLE suggestion (
+    id SERIAL PRIMARY KEY,
+    suggestion text NOT NULL,
+    bettor integer REFERENCES bettor
+);
+
+CREATE TABLE team (
+    id SERIAL PRIMARY KEY,
+    sport varchar(50) NOT NULL,
+    league varchar(100) NOT NULL,
+    name varchar(200) NOT NULL
+);
+
+
+CREATE TABLE matchup (
+    id SERIAL PRIMARY KEY,
+    startdate varchar(50) NOT NULL,
+    scorehome integer DEFAULT 0 NOT NULL,
+    scoreaway integer DEFAULT 0 NOT NULL,
+    result varchar(10),
+    betting_result varchar(1)
+);
+
+CREATE TABLE matchup_team (
+    matchup integer REFERENCES matchup,
+    team integer REFERENCES team,
+    hometeam boolean DEFAULT true
+);
+
+CREATE TABLE wager (
+    bettor integer REFERENCES bettor NOT NULL,
+    matchup integer REFERENCES matchup NOT NULL,
+    betting_choice varchar(1) NOT NULL,
+    betting_amount float NOT NULL
+);
