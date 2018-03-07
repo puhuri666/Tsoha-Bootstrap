814acac2cb60f8a225cab01ef3e83c0f3031b18b
puhuri666 <pmarkkola@gmail.com>
2018-03-07 14:03:29 +0200

    environment

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="pmarkkol"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsohabootstrap"
+PROJECT_FOLDER="potti"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
